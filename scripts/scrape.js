c048f2d38140b74509056300d9ec8e6cb7df2f95
gunner-uw <gvd@uw.edu>
2024-06-26 14:57:14 -0700

    added scraping for massdevice pg1

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,32 +1,39 @@
-// scripts/scrape.js
+'use strict';
+
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
 (async () => {
-  // Launch the browser
+
+  // browser launch
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Go to the website
-  await page.goto('https://example.com');
+  await page.goto('https://www.massdevice.com/?s=stryker&page=1');
 
-  // Perform scraping
+
   const data = await page.evaluate(() => {
-    const items = [];
-    document.querySelectorAll('div').forEach(item => {
-      items.push({
-        title: item.querySelector('h1').innerText,
-        description: item.querySelector('p').innerText
-      });
+    const articles = [];
+
+    const articleContainers = document.querySelectorAll('.search-results-article-container');
+
+    // iterate article containers
+    articleContainers.forEach(container => {
+      const timestamp = container.querySelector('.search-results-article--timestamp').textContent.trim();
+      const title = container.querySelector('.search-results-article--title a').textContent.trim();
+
+      // push data to json element
+      articles.push({ title, timestamp });
     });
-    return items;
+
+    return articles;
   });
 
-  // Save data to a file
+  // save to JSON
   const filePath = path.join(__dirname, '../data/scraped-data.json');
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 
-  // Close the browser
+  // close
   await browser.close();
 })();
